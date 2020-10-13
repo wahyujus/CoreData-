7bff7d5a2926adfd75d7fc48d06fae6a516a0d7a
wahyujus <wahyukharismaz@gmail.com>
2020-10-13 19:06:46 +0700

    Initial Commit

---
+++ jus4k/jus4kApp.swift
@@ -0,0 +1,17 @@
+//
+//  jus4kApp.swift
+//  jus4k
+//
+//  Created by wahyujus on 13/10/20.
+//
+
+import SwiftUI
+
+@main
+struct jus4kApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
